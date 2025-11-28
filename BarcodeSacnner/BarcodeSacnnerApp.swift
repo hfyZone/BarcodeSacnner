3eabfb3759c2c188b1eefe0d8273ef65d9c0fa78
hfyZone <1178805691@qq.com>
2025-11-28 12:23:36 +0800

    Initial Commit

---
+++ BarcodeSacnner/BarcodeSacnnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeSacnnerApp.swift
+//  BarcodeSacnner
+//
+//  Created by 韩飞洋 on 2025/11/28.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeSacnnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
